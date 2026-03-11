9cf867e7110da6e233af0cb3e6dc6b54ec66417d
Anilsh007 <anil.sh101@gmail.com>
2026-03-11 17:50:05 +0530

    Update next config for static export

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Mera-Dukan-Mera-Godam', // Aapka repo name '/' ke saath
-  images: { unoptimized: true },
+  basePath: '/Mera-Dukan-Mera-Godam',
+  images: {
+    unoptimized: true,
+  },
+  // experimental ke andar images mat rakhiye
 };
-
 
 export default nextConfig;
