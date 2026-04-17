0bdd711daae88105bd8449063f1914c107ae40be
Anilsh007 <anil.sh101@gmail.com>
2026-04-17 18:15:13 +0530

    Improve UI

--- app/components/client/useClient.js
+++ app/components/client/useClient.js
@@ -3,10 +3,10 @@
 
 // import aur export all components from a single file
 export { default as Custombutton } from "../utility/Button";
-export { default as Header } from "../reuseModule/Header";
+export { default as Header } from "../common/Header";
 export { default as Login } from "../reuseModule/login";
 export { default as ProtectedRoute } from "../reuseModule/ProtectedRoute";
-export { default as Sidebar } from "../reuseModule/Sidebar";
+export { default as Sidebar } from "../common/Sidebar";
 export { auth, provider } from "../../lib/firebase";
 export { db } from "../../lib/db";
 export { default as Button } from "../utility/Button";
